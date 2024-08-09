30b265d8d4a8a086babbf10fcbb1fea8940e1d28
A-Hassan-M <a.hassanmahmoud0@gmail.com>
2024-08-09 06:44:03 +0300

    Added unit tests for Authentication layer and UserService

---
+++ src/test/java/com/qeema/practicaltest/ordermanagement/OrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qeema.practicaltest.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
